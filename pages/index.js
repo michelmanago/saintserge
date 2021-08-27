996978cb9a5320bf85f61387ef3f55ac2275cd91
KenyZ <zkeny@outlook.fr>
2021-08-27 09:52:51 +0200

    wip

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@
 import React from "react";
 import Image from 'next/image'
 import Link from "next/link"
+import SwiperCore, { Navigation, A11y } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // styles
 import styles from "../styles/pages/home.module.css"
 import 'swiper/swiper-bundle.css';
 
+SwiperCore.use([Navigation, A11y])
 const lorem = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ad modi, culpa doloribus in eum dolores veniam repellendus architecto aspernatur provident ea nulla amet. Quis!"
 
 const Home = () => {
@@ -30,16 +32,22 @@ const Home = () => {
 
               {/* Text */}
               <div className="w-1/2 mr-10">
-                <p className="block text-xl font-normal mb-5 leading-8">{lorem}</p>
-                <p className="block text-xl font-serif text-lg leading-">{lorem.repeat(3)}</p>
+                <p className="block text-2xl font-normal mb-10 leading-8">{lorem}</p>
+                <p className="block text-2xl font-serif mb-5 text-lg leading-">{lorem.repeat(3)}</p>
                 <Link href="#">
                   <a className="inline-flex justify-center items-center w-32 h-10 border-2 border-plightmaroon text-xs mt-10 uppercase">En savoir plus</a>
                 </Link>
               </div>
 
               {/* Image */}
-              <div className="">
-                <img className="" src="https://i.picsum.photos/id/499/600/600.jpg?hmac=8DVR1iJ06_QxDOBazkF1SWTM6fyRennwVQf2ebe27_k" alt="" />
+              <div className="w-1/2 max-w-1/2">
+                <Carousel
+                  images={[
+                    "https://i.picsum.photos/id/499/600/600.jpg?hmac=8DVR1iJ06_QxDOBazkF1SWTM6fyRennwVQf2ebe27_k",
+                    "https://i.picsum.photos/id/499/600/600.jpg?hmac=8DVR1iJ06_QxDOBazkF1SWTM6fyRennwVQf2ebe27_k",
+                    "https://i.picsum.photos/id/499/600/600.jpg?hmac=8DVR1iJ06_QxDOBazkF1SWTM6fyRennwVQf2ebe27_k",
+                  ]}
+                />
               </div>
             </div>
           </div>
@@ -48,22 +56,23 @@ const Home = () => {
   );
 };
 
-const Carousel = ({}) => {
+const Carousel = ({images}) => {
   return (
     <Swiper
       // install Swiper modules
-      // modules={[Navigation, Pagination, Scrollbar, A11y]}
       spaceBetween={50}
-      slidesPerView={3}
-      // navigation
+      slidesPerView={1}
+      navigation={{
+
+      }}
       onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log('slide change')}
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      ...
+      {images.map((src, index) => (
+        <SwiperSlide key={src + index}>
+          <img className="w-full" src={src} alt="" />
+        </SwiperSlide>
+      ))}
     </Swiper>
   )
 }
