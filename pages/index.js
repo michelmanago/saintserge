e7a9ef24a27e0a54b50a74f4e2b71fd5b6d7cf42
Guillaume Lengrand <guillaume.lengrand@gmail.com>
2022-01-12 12:06:52 +0100

    News: add component to display last news in index page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
 // libs
 import { useRouter } from "next/router";
 import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
-import { getProperDate } from "../lib/date";
 
 // components
 import Header from "../components/header/header";
@@ -20,6 +17,7 @@ import {
   getServerImageEndpoint,
 } from "../utils/utils-serveur-image";
 import { getSingleMedia } from "../model/media";
+import LastNews from "../components/last-news";
 
 export default function Home({ menu, articles }) {
   const router = useRouter();
@@ -49,39 +47,7 @@ export default function Home({ menu, articles }) {
           </div>
         </header>
         <AppHome currentLanguage={locale} />
-
-        <div className="bg-pwhite">
-          <div className="container max-w-screen-xl bg-white sm:mx-auto">
-            <h2 className="px-5 pt-4 sm:px-48">Actualités</h2>
-            <div className="flex flex-wrap p-2">
-              {articles?.map((article) => (
-                <div className="w-1/3 px-2" key={article.id}>
-                  <Link href={`/${article.pageSlug}`}>
-                    <a>
-                      <div className="relative">
-                        <img
-                          className="mx-auto max-h-56"
-                          src={`${article.image}`}
-                        />
-                      </div>
-                      <div className="text-center">
-                        <h3 className="inline-block">{article.pageName}</h3>
-                        <div className="inline-block mx-2 text-sm">
-                          {getProperDate(article.created_at)}
-                        </div>
-                      </div>
-                    </a>
-                  </Link>
-                </div>
-              ))}
-              {/*<div className="w-full mt-3 text-center">
-                        <Link href="/articles">
-                            <a className='px-2 py-3 text-white cursor-pointer bg-pgold'>Toutes les Actualités</a>
-                        </Link>
-                                        </div>*/}
-            </div>
-          </div>
-        </div>
+        <LastNews articles={articles} />
       </div>
     </div>
   );
