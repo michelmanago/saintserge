df242bf822f8593caf8d3816053fb0119ee5d093
michelmanago <45874607+michelmanago@users.noreply.github.com>
2022-01-12 11:35:57 +0100

    modifié la page d'accueil

--- components/apphome.js
+++ components/apphome.js
@@ -8,7 +8,7 @@ export default function AppHome () {
         <div className="bg-pwhite">
             <div className="container sm:mx-auto bg-white max-w-screen-xl">
                 <main className="bg-white pt-4 px-5 sm:px-48">
-                   <h2 className="">La colline saint-serge</h2>
+                   <h2 className="">La colline Saint-Serge</h2>
  
                     <p className="pb-2">{t("home:paragraph_1")}</p>
                     <p className="pb-2">{t("home:paragraph_2")}</p>
