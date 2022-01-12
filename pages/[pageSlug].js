e5023e148b5d23040dba4255af76de56b0c7081c
Guillaume Lengrand <guillaume.lengrand@gmail.com>
2022-01-12 13:35:00 +0100

    PageSlug: don't display bandeau image and sideBar for articles category page

--- pages/[pageSlug].js
+++ pages/[pageSlug].js
@@ -1,106 +1,86 @@
-
-
 // libs
-import dynamic from 'next/dynamic';
-import { useEffect } from 'react';
-import DefaultErrorPage from 'next/error'
-import Head from "next/head"
+import dynamic from "next/dynamic";
+import { useEffect } from "react";
+import DefaultErrorPage from "next/error";
+import Head from "next/head";
 
 // component
-import Header from '../components/header/header';
-import PageDefault from '../components/page-template/PageDefault';
-import PageWithCategory from '../components/page-template/PageWithCategory';
+import Header from "../components/header/header";
+import PageDefault from "../components/page-template/PageDefault";
+import PageWithCategory from "../components/page-template/PageWithCategory";
 
 // model
-import {getMenu} from '../model/menu';
-import { getPageBySlug } from '../model/page';
+import { getMenu } from "../model/menu";
+import { getPageBySlug } from "../model/page";
 
 // utils
-import { getMediaLink } from '../utils/utils-serveur-image';
+import { getMediaLink } from "../utils/utils-serveur-image";
 
-const NavCompositeur = dynamic(() => import('../components/compositeurs/nav'));
-
+const NavCompositeur = dynamic(() => import("../components/compositeurs/nav"));
 
 // styles
 const bannerStyles = {
-    height: 360
-}
+  height: 360,
+};
 
+export default function DynPage({ menu, page }) {
+  // redirect 404
+  if (!page) {
+    return <DefaultErrorPage statusCode={404} />;
+  }
 
+  // Lifecycle
+  useEffect(() => {
+    // hack for dev
+    window.EDIT_THIS_PAGE = `${window.location.origin}/admin/page/${page.originalPageId}`;
+  }, []);
 
-export default function DynPage({ menu, page}) {
-    
-    // redirect 404
-    if(!page){
-        return <DefaultErrorPage statusCode={404} />
+  // utils
+  const renderPage = (page) => {
+    const hasCategory = !!page.page;
+
+    if (hasCategory && page.page != "articles") {
+      return <PageWithCategory key={page.id} page={page} />;
+    } else {
+      return <PageDefault key={page.id} page={page} />;
     }
+  };
 
-    // Lifecycle
-    useEffect(() => {
+  // hooks
 
-        // hack for dev
-        window.EDIT_THIS_PAGE = `${window.location.origin}/admin/page/${page.originalPageId}`
-        
-    }, [])
+  return (
+    <div>
+      <Head>
+        <title>{page && page.pageName}</title>
+      </Head>
 
+      <Header menu={menu.data} translations={page.translations} />
 
-    // utils
-    const renderPage = page => {
-
-        const hasCategory = !!page.page
-
-
-        if(hasCategory){
-            return <PageWithCategory key={page.id} page={page}/>
-        } 
-
-        else {
-            return <PageDefault key={page.id} page={page}/>
-        }
-
-    }
-
-    // hooks
-
-    return (
-        <div>
-            <Head>
-                <title>{page && page.pageName}</title>
-                
-            </Head>
-
-            <Header menu={menu.data} translations={page.translations}/>
-
-            {/* Banner */}
-            {page.bandeau && (
-                <div style={bannerStyles} className="">
-                    <img
-                        className="block object-cover w-full h-full"
-                        src={getMediaLink(page.bandeau.public_path)}
-                        alt=""
-                    />
-                </div>
-            )}
-
-            <main className="max-w-screen-xl px-10 py-10 mx-auto bg-white border">
-
-                {
-                    renderPage(page)
-                }
-                
-            </main>
-
+      {/* Banner */}
+      {page.bandeau && page.page != "articles" && (
+        <div style={bannerStyles} className="">
+          <img
+            className="block object-cover w-full h-full"
+            src={getMediaLink(page.bandeau.public_path)}
+            alt=""
+          />
         </div>
-    );
+      )}
+
+      <main className="max-w-screen-xl px-10 py-10 mx-auto bg-white border">
+        {renderPage(page)}
+      </main>
+    </div>
+  );
 }
 
-export async function getStaticPaths({locales}) {
-    // Call an external API endpoint to get posts
-    //const defunts = await getDefunts(100, 0);
-    //const posts = await res.json();
+export async function getStaticPaths({ locales }) {
+  // Call an external API endpoint to get posts
+  //const defunts = await getDefunts(100, 0);
+  //const posts = await res.json();
 
-    // Get the paths we want to pre-render based on posts
-    /*const paths = defunts.data.map(
+  // Get the paths we want to pre-render based on posts
+  /*const paths = defunts.data.map(
         defunt => (
             {
                 params: {id: defunt.id.toString()},
@@ -116,28 +96,28 @@ export async function getStaticPaths({locales}) {
             }
         ),
     );*/
-    const paths = [];
+  const paths = [];
 
-    // We'll pre-render only these paths at build time.
-    // { fallback: false } means other routes should 404.
-    return {paths, fallback: 'blocking'};
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: "blocking" };
 }
 
 export async function getStaticProps(context) {
+  const { pageSlug } = context.params;
 
-    const {pageSlug} = context.params;
+  const menu = await getMenu(context.locale);
+  const page = await getPageBySlug(
+    context.locale + "/" + pageSlug,
+    "render"
+  ).catch((err) => null);
 
-    const menu = await getMenu(context.locale);
-    const page = await getPageBySlug(context.locale + "/" + pageSlug, "render").catch(err => null);
+  return {
+    props: {
+      page,
+      menu,
+    },
 
-    return {
-        props: {
-            page, menu 
-        },
-        
-        revalidate: 10,
-    };
-    
-    
+    revalidate: 10,
+  };
 }
-
