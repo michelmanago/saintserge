f48724d08bd13e98806c3fb416a1c731e443b6b5
michelmanago <45874607+michelmanago@users.noreply.github.com>
2022-01-13 16:28:36 +0100

    force a rebuild because too slow

--- components/apphome.js
+++ components/apphome.js
@@ -9,7 +9,6 @@ export default function AppHome () {
             <div className="container sm:mx-auto bg-white max-w-screen-xl">
                 <main className="bg-white pt-4 px-5 sm:px-10">
                    <h2 className="">La colline Saint-Serge</h2>
- 
                     <p className="pb-2">{t("home:paragraph_1")}</p>
                     <p className="pb-2">{t("home:paragraph_2")}</p>
                 </main>
