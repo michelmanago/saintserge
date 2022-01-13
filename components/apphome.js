ad22499c6606d1ddcff4e42936136d8737662803
michelmanago <45874607+michelmanago@users.noreply.github.com>
2022-01-13 16:05:49 +0100

    modifié les marges des 1er paragraphes sur la page d'accueil

--- components/apphome.js
+++ components/apphome.js
@@ -7,7 +7,7 @@ export default function AppHome () {
     return (
         <div className="bg-pwhite">
             <div className="container sm:mx-auto bg-white max-w-screen-xl">
-                <main className="bg-white pt-4 px-5 sm:px-48">
+                <main className="bg-white pt-4 px-5 sm:px-10">
                    <h2 className="">La colline Saint-Serge</h2>
  
                     <p className="pb-2">{t("home:paragraph_1")}</p>
