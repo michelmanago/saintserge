82e787b5d29d061fa44cf3db297d6e2d9213c53e
Guillaume Lengrand <guillaume.lengrand@gmail.com>
2022-05-23 13:29:31 +0200

    Work block: add popup to delete item, add color on select status

--- components/page-editor/blocks/worksEditor.js
+++ components/page-editor/blocks/worksEditor.js
@@ -2,8 +2,10 @@ import {useEffect, useState} from 'react';
 import IconArrowUp from '../../icons/IconArrowUp';
 import IconArrowDown from '../../icons/IconArrowDown';
 import Trash from '../../icons/trash';
+import Popup from 'reactjs-popup';
 
 export default function WorksEditor({content, setContent, originalPageId, addAttributedMedia}) {
+    const [popupOpen, setPopupOpen] = useState(false);
     const addWork = () => {
         let newContent = [...content];
         newContent.push({
@@ -34,7 +36,13 @@ export default function WorksEditor({content, setContent, originalPageId, addAtt
                                 />
                                 <div className="flex flex-col gap-2">
                                     <select
-                                        className="px-1 mx-1 border rounded"
+                                        className={`px-1 mx-1 border rounded text-white ${
+                                            work.status === 'toDo'
+                                                ? 'bg-red-500'
+                                                : work.status === 'Done'
+                                                ? 'bg-green-500'
+                                                : 'bg-yellow-500'
+                                        }`}
                                         value={work.status}
                                         onChange={e => {
                                             e.preventDefault();
@@ -47,24 +55,61 @@ export default function WorksEditor({content, setContent, originalPageId, addAtt
                                         <option value={'workInProgess'}>En cours</option>
                                         <option value={'Done'}>Réalisé</option>
                                     </select>
-                                    <button
-                                        className="px-2 py-1 text-white bg-red-500 rounded hover:bg-red-600"
-                                        onClick={e => {
-                                            e.preventDefault();
-                                            if (window.confirm('voulez vous vraiment supprimer cet élément ?')) {
-                                                let newContent = [...content];
-                                                newContent.splice(index, 1);
-                                                setContent(newContent);
-                                            }
-                                        }}
-                                    >
-                                        Supprimer
-                                    </button>
                                 </div>
                             </div>
                         ))}
                 </div>
-                <div className="place-self-center">
+                <Popup lockScroll={true} open={popupOpen} onClose={() => setPopupOpen(false)}>
+                    <div className="flex flex-col gap-1">
+                        <div className="flex flex-row-reverse">
+                            <button
+                                className="p-1 text-white bg-red-500 rounded hover:bg-red-600"
+                                onClick={() => setPopupOpen(false)}
+                            >
+                                Fermer
+                            </button>
+                        </div>
+                        {content &&
+                            content.length > 0 &&
+                            content.map((work, index) => (
+                                <div className="flex flex-row items-center" key={index}>
+                                    <textarea
+                                        className="w-full px-1 mx-1 border rounded"
+                                        value={work.label}
+                                        onChange={e => {
+                                            e.preventDefault();
+                                            let newContent = [...content];
+                                            newContent[index].label = e.target.value;
+                                            setContent(newContent);
+                                        }}
+                                        id="label"
+                                    />
+                                    <div className="flex flex-col gap-2">
+                                        <button
+                                            className="px-2 py-1 text-white bg-red-500 rounded hover:bg-red-600"
+                                            onClick={e => {
+                                                e.preventDefault();
+                                                if (window.confirm('voulez vous vraiment supprimer cet élément ?')) {
+                                                    let newContent = [...content];
+                                                    newContent.splice(index, 1);
+                                                    setContent(newContent);
+                                                }
+                                            }}
+                                        >
+                                            Supprimer
+                                        </button>
+                                    </div>
+                                </div>
+                            ))}
+                    </div>
+                </Popup>
+                <div className="flex flex-col gap-2 place-self-center">
+                    <button
+                        className="px-2 py-1 text-white bg-red-500 rounded hover:bg-red-600"
+                        onClick={() => setPopupOpen(true)}
+                    >
+                        Supprimer des éléments
+                    </button>
                     <button
                         className="px-2 py-1 text-white bg-blue-700 rounded hover:bg-blue-800"
                         onClick={e => {
