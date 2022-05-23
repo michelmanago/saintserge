8d485a2a82c7661a3175392ecbb1bcfd7b8942ac
Guillaume Lengrand <guillaume.lengrand@gmail.com>
2022-05-23 14:03:00 +0200

    Work Block: add traffic light

--- components/page-template/commons/worksBlock.jsx
+++ components/page-template/commons/worksBlock.jsx
@@ -6,13 +6,16 @@ const WorksBlock = ({works}) => {
         <div>
             {works && works.filter(work => work.status === 'Done').length != 0 && (
                 <div>
-                    <h2>
-                        {locale === 'fr'
-                            ? 'Travaux réalisés'
-                            : locale === 'en'
-                            ? 'Work Completed'
-                            : 'Осуществленные работы'}
-                    </h2>
+                    <div className="flex flex-row items-center gap-2">
+                        <img src="/static/img/feu_tricolore_green.png" className="h-16" />
+                        <h2>
+                            {locale === 'fr'
+                                ? 'Travaux réalisés'
+                                : locale === 'en'
+                                ? 'Work Completed'
+                                : 'Осуществленные работы'}
+                        </h2>
+                    </div>
                     <ul>
                         {works &&
                             works
@@ -23,13 +26,16 @@ const WorksBlock = ({works}) => {
             )}
             {works && works.filter(work => work.status === 'workInProgess').length != 0 && (
                 <div>
-                    <h2>
-                        {locale === 'fr'
-                            ? 'Travaux en cours'
-                            : locale === 'en'
-                            ? 'Work in Progress'
-                            : 'Ведущиеся работы'}
-                    </h2>
+                    <div className="flex flex-row items-center gap-2">
+                        <img src="/static/img/feu_tricolore_yellow.png" className="h-16" />
+                        <h2>
+                            {locale === 'fr'
+                                ? 'Travaux en cours'
+                                : locale === 'en'
+                                ? 'Work in Progress'
+                                : 'Ведущиеся работы'}
+                        </h2>
+                    </div>
                     <ul>
                         {works
                             .filter(work => work.status === 'workInProgess')
@@ -42,13 +48,16 @@ const WorksBlock = ({works}) => {
 
             {works && works.filter(work => work.status === 'toDo').length != 0 && (
                 <div>
-                    <h2>
-                        {locale === 'fr'
-                            ? 'Travaux à faire'
-                            : locale === 'en'
-                            ? 'Work to Be Done'
-                            : 'Необходимые работы, которые осталось провести'}
-                    </h2>
+                    <div className="flex flex-row items-center gap-2">
+                        <img src="/static/img/feu_tricolore_red.png" className="h-16" />
+                        <h2>
+                            {locale === 'fr'
+                                ? 'Travaux à faire'
+                                : locale === 'en'
+                                ? 'Work to Be Done'
+                                : 'Необходимые работы, которые осталось провести'}
+                        </h2>
+                    </div>
                     <ul>
                         {works
                             .filter(work => work.status === 'toDo')
