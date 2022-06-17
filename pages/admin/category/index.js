78f82dca0fae62fb4a1a19f5728496be43aec570
Guillaume Lengrand <guillaume.lengrand@gmail.com>
2022-06-17 12:04:39 +0200

    Category: remove home and articles from order editor

--- pages/admin/category/index.js
+++ pages/admin/category/index.js
@@ -1,26 +1,23 @@
-
 // models
-import { getMenu } from '../../../model/menu';
+import {getMenu} from '../../../model/menu';
 
 // libs
 import Head from 'next/head';
 import {getSession} from 'next-auth/client';
-import Link from "next/link"
+import Link from 'next/link';
 
 // components
 import Header from '../../../components/header/header';
 
 // parameters
-import { capitalize } from '../../../utils/utils';
-import { getAllCategories } from '../../../model/category';
+import {capitalize} from '../../../utils/utils';
+import {getAllCategories} from '../../../model/category';
 
 export default function AdminCategory({menu, categories}) {
-
     return (
         <>
             <Head>
                 <title>Admin - Catégories</title>
-                
             </Head>
 
             {menu && <Header menu={menu.data} />}
@@ -28,15 +25,17 @@ export default function AdminCategory({menu, categories}) {
             <main className="max-w-screen-xl p-4 bg-white md:mx-auto">
                 <h1 className="mb-3 text-4xl font-semibold">Modifier les catégories</h1>
                 <ul className="pl-5">
-                    {
-                        categories.map(cat => (
-                            <li key={cat.id}>
-                                <Link href={`/admin/category/${cat.title}`}>
-                                    <a className="text-lg text-blue-500 underline">{capitalize(cat.title)}</a>
-                                </Link>
-                            </li>
-                        ))
-                    }
+                    {categories.map(
+                        cat =>
+                            cat.title != 'home' &&
+                            cat.title != 'articles' && (
+                                <li key={cat.id}>
+                                    <Link href={`/admin/category/${cat.title}`}>
+                                        <a className="text-lg text-blue-500 underline">{capitalize(cat.title)}</a>
+                                    </Link>
+                                </li>
+                            ),
+                    )}
                 </ul>
             </main>
         </>
@@ -57,12 +56,13 @@ export async function getServerSideProps(context) {
     }
 
     // data
-    const menu = await getMenu(context.locale)
-    const categories = await getAllCategories()
+    const menu = await getMenu(context.locale);
+    const categories = await getAllCategories();
 
-  
-    return {props: {
-      menu: menu,
-      categories,
-    }}
+    return {
+        props: {
+            menu: menu,
+            categories,
+        },
+    };
 }
