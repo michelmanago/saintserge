3ece0008bdd0f4a1ec1eb06bd8bc23f70a39c95f
Guillaume Lengrand <guillaume.lengrand@gmail.com>
2023-10-16 16:09:11 +0200

    Feat: add draft filter on article

--- components/last-news.js
+++ components/last-news.js
@@ -12,34 +12,36 @@ export default function LastNews({articles}) {
                 <div className="px-5 pt-4">
                     <h2>{locale === 'fr' ? 'Actualités' : locale === 'en' ? 'News' : 'Новости'}</h2>
                     <div className="flex flex-wrap p-2">
-                        {articles?.map(article => (
-                            <>
-                                <div className="w-full px-2 mt-2 sm:w-1/2 md:w-1/3" key={article.id}>
-                                    <Link href={`/${article.pageSlug}`}>
-                                        <a className="flex flex-col gap-1">
-                                            {article.image ? (
-                                                <img
-                                                    className="object-cover mx-auto"
-                                                    style={{aspectRatio: '4 / 3'}}
-                                                    src={`${article.image}`}
-                                                />
-                                            ) : null}
-                                            <div className="font-serif text-xl font-bold sm:text-2xl">
-                                                {article.pageName}
-                                            </div>
-                                            <div className="inline-block text-sm">
-                                                {locale === 'fr'
-                                                    ? 'Publié le'
-                                                    : locale === 'en'
-                                                    ? 'Published'
-                                                    : 'Опубликовано'}{' '}
-                                                {getProperDate(article.created_at)}
-                                            </div>
-                                        </a>
-                                    </Link>
-                                </div>
-                            </>
-                        ))}
+                        {articles
+                            ?.filter(a => !a.draft)
+                            .map(article => (
+                                <>
+                                    <div className="w-full px-2 mt-2 sm:w-1/2 md:w-1/3" key={article.id}>
+                                        <Link href={`/${article.pageSlug}`}>
+                                            <a className="flex flex-col gap-1">
+                                                {article.image ? (
+                                                    <img
+                                                        className="object-cover mx-auto"
+                                                        style={{aspectRatio: '4 / 3'}}
+                                                        src={`${article.image}`}
+                                                    />
+                                                ) : null}
+                                                <div className="font-serif text-xl font-bold sm:text-2xl">
+                                                    {article.pageName}
+                                                </div>
+                                                <div className="inline-block text-sm">
+                                                    {locale === 'fr'
+                                                        ? 'Publié le'
+                                                        : locale === 'en'
+                                                        ? 'Published'
+                                                        : 'Опубликовано'}{' '}
+                                                    {getProperDate(article.created_at)}
+                                                </div>
+                                            </a>
+                                        </Link>
+                                    </div>
+                                </>
+                            ))}
                         <div className="flex flex-row justify-center w-full mt-2">
                             <Link href="/articles" locale={locale}>
                                 <a className="p-2 text-white rounded cursor-pointer bg-pred">
