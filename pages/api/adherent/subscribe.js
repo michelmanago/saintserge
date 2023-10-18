f27b5030df185e170871a89e3d2e811449ed86d5
Guillaume Lengrand <guillaume.lengrand@gmail.com>
2023-10-18 13:12:41 +0200

    Feat: add input to subscribe to the new news

--- pages/api/adherent/subscribe.js
+++ pages/api/adherent/subscribe.js
@@ -1,16 +1,16 @@
-import {unsubscribeAdherent} from '../../../dao/adherent';
+import {createAdherent} from '../../../dao/adherent';
 
 export default async function handler(req, res) {
     if (req.method === 'GET') {
         let emailUser = req.query?.email;
         console.log({emailUser});
         try {
-            await unsubscribeAdherent(emailUser);
+            await createAdherent({email: emailUser});
         } catch (error) {
             return res.status(500).json({error});
         }
         // sendEmailNewArticle(page);
-        return res.json({message: 'unsubscribe'});
+        return res.json({message: 'subscribe'});
     } else {
         return res.status(400).json({message: 'Method not allow'});
     }
